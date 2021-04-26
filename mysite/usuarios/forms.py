a7b89dfbbc6662fb641e4b1e1083ef50c6b6c2eb
GuidoPadilla <sebaspadilla2010@hotmail.com>
2021-04-25 18:04:40 -0600

    create login page and test page

---
+++ mysite/usuarios/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+
+class AuthenticationAddForm(forms.Form):
+    username = forms.CharField(label="Nombre de Usuario", max_length=150)
+    password = forms.CharField(label="Contraseña", max_length=150, widget=forms.PasswordInput)
