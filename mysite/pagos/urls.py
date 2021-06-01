983510a0037f0268853f3a41675083d67aca095c
GuidoPadilla <sebaspadilla2010@hotmail.com>
2021-06-01 15:30:56 -0600

    table pagos final updates

--- mysite/pagos/urls.py
+++ mysite/pagos/urls.py
@@ -6,5 +6,6 @@ app_name = "pagos"
 urlpatterns = [
     path('control/', views.control_view, name='pagos_control'),
     path('ingreso/', views.ingreso_view, name='pagos_ingreso'),
-    path('pagos/', views.pagos, name='lista_pagos')
+    path('pagos/', views.pagos, name='lista_pagos'),
+    path('pagos_excel.xls', views.control_pagos, name='pagos_excel')
 ]
