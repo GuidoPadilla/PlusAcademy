57d7b652bba0ecf84aa3d429f5fe0edc6addb98b
Luis Quezada <que18028@uvg.edu.gt>
2021-07-20 19:01:32 -0600

    Done all URL tests

--- mysite/pagos/tests.py
+++ mysite/pagos/tests.py
@@ -24,3 +24,44 @@ class TestUrls(SimpleTestCase):
 		url = reverse('pagos:lista_pagos')
 		print(resolve(url))
 		self.assertEquals(resolve(url).func,pagos)
+
+	def test_url_lista_saldos(self):
+		url = reverse('pagos:lista_saldos')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,saldos)
+
+	def test_url_pagos_excel(self):
+		url = reverse('pagos:pagos_excel')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,control_pagos)
+
+	def test_url_solicitar_eliminacion(self):
+		url = reverse('pagos:solicitar_eliminacion')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,solicitarEliminacionPago)
+
+	def test_url_pagos_solicitados_eliminar(self):
+		url = reverse('pagos:pagos_solicitados_eliminar')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,pagos_solicitados_eliminar_view)
+
+	def test_url_pagos_pagos_pendientes(self):
+		url = reverse('pagos:pagos_pendientes')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,pagos_pendientes)
+
+	def test_url_solicitud_eliminacion_pago(self):
+		url = reverse('pagos:solicitud_eliminacion_pago')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,solicitud_eliminacion_pago)
+
+	def test_url_pagos_eliminados(self):
+		url = reverse('pagos:pagos_eliminados')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,pagos_eliminados)
+
+	def test_url_pagos_eliminados_list(self):
+		url = reverse('pagos:pagos_eliminados_list')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,pagos_eliminados_list)
+			
