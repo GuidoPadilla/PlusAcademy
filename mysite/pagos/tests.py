7a347eb99ba17bb0ff31919f7d1f0772e648e2ce
Luis Quezada <que18028@uvg.edu.gt>
2021-07-20 18:35:35 -0600

    bug fix in URL tests

--- mysite/pagos/tests.py
+++ mysite/pagos/tests.py
@@ -6,21 +6,21 @@ from pagos.views import control_view, saldo_view, ingreso_view, pagos, saldos, c
 class TestUrls(SimpleTestCase):
 
 	def test_url_pagos_control(self):
-		url = reverse('pagos_control')
+		url = reverse('pagos:pagos_control')
 		print(resolve(url))
 		self.assertEquals(resolve(url).func,control_view)
 
 	def test_url_pagos_saldo(self):
-		url = reverse('pagos_saldo')
+		url = reverse('pagos:pagos_saldo')
 		print(resolve(url))
 		self.assertEquals(resolve(url).func,saldo_view)
 
 	def test_url_pagos_ingreso(self):
-		url = reverse('pagos_ingreso')
+		url = reverse('pagos:pagos_ingreso')
 		print(resolve(url))
 		self.assertEquals(resolve(url).func,ingreso_view)
 
 	def test_url_lista_pagos(self):
-		url = reverse('lista_pagos')
+		url = reverse('pagos:lista_pagos')
 		print(resolve(url))
 		self.assertEquals(resolve(url).func,pagos)
