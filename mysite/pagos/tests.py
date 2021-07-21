5f8f8446e565188cf694409cf7bfad1130d025cf
Luis Quezada <que18028@uvg.edu.gt>
2021-07-20 18:15:38 -0600

    Added 4 URL Unit tests

--- mysite/pagos/tests.py
+++ mysite/pagos/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse, resolve
+from pagos.views import control_view, saldo_view, ingreso_view, pagos, saldos, control_pagos, solicitarEliminacionPago, pagos_solicitados_eliminar_view, pagos_pendientes, solicitud_eliminacion_pago, pagos_eliminados, pagos_eliminados_list
+
+class TestUrls(SimpleTestCase):
+
+	def test_url_pagos_control(self):
+		url = reverse('pagos_control')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,control_view)
+
+	def test_url_pagos_saldo(self):
+		url = reverse('pagos_saldo')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,saldo_view)
+
+	def test_url_pagos_ingreso(self):
+		url = reverse('pagos_ingreso')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,ingreso_view)
+
+	def test_url_lista_pagos(self):
+		url = reverse('lista_pagos')
+		print(resolve(url))
+		self.assertEquals(resolve(url).func,pagos)
