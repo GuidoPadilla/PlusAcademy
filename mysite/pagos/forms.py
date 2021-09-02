e916c35564eaeaa7f018a7c3282e83eacde3227e
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-09-01 22:12:55 -0600

    cobro extra curso

--- mysite/pagos/forms.py
+++ mysite/pagos/forms.py
@@ -1,9 +1,11 @@
+
 from django.forms import ModelForm, Form
 from django import forms
 from django.conf import settings
 from django.contrib.auth.models import User
 from pagos.models import Pago, Cobro, TipoPago
-from usuarios.models import Moneda
+from usuarios.models import Moneda, Curso
+
 
 class PaymentRegisterForm(ModelForm):
 
@@ -25,7 +27,20 @@ class CobroExtraForm(Form):
     tipo_moneda = forms.ModelChoiceField(queryset=Moneda.objects.all())
     class Meta:
         model = Cobro
-        # fields = ['user', 'fecha_cobro', 'monto', 'tipo_pago', 'tipo_moneda']
+        
+
+    def __init__(self, *args, **kwrgs):
+        super(CobroExtraForm, self).__init__(*args, **kwrgs)
+        self.fields['user'].queryset = User.objects.filter(userextra__rol__nombre='estudiante')
+
+class CobroExtraCursoForm(Form):
+    Curso = forms.ModelChoiceField(queryset= Curso.objects.all())
+    fecha_cobro = forms.DateField(widget=forms.DateInput(format='%d/%m/%Y'), input_formats=settings.DATE_INPUT_FORMATS)
+    monto = forms.FloatField()
+    tipo_pago = forms.ModelChoiceField(queryset=TipoPago.objects.all())
+    tipo_moneda = forms.ModelChoiceField(queryset=Moneda.objects.all())
+    class Meta:
+        model = Cobro
         
 
     def __init__(self, *args, **kwrgs):
